8701270792ef535b2109e779ff2dc96ed46e8c9b
avsnisar <avsnisar@ukr.net>
2019-07-12 16:32:08 +0300

    moved tests to separate files

--- test/test_login.py
+++ test/test_login.py
@@ -1,6 +1,5 @@
 import pytest
 from fixture.application import Application
-from model.film import Film
 
 
 @pytest.fixture
@@ -12,12 +11,6 @@ def app(request):
 
 def test_login_logout(app):
     app.session.login("admin", "admin")
-    app.session.logout()
-
-
-def test_add_movie(app):
-    app.session.login("admin", "admin")
-    app.movie.create(Film(name="Test", year="1234", description="Goes here"))
     app.session.logout()
 
 
