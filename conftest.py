d5c14c05a1e043dfab834f77ccbc6392a481cafc
avsnisar <avsnisar@ukr.net>
2019-07-12 18:31:28 +0300

    added a teardown fixture and validation to back-up if browser failed

--- conftest.py
+++ conftest.py
@@ -1,12 +1,21 @@
 import pytest
 from fixture.application import Application
 
+fixture = None
 
-@pytest.fixture(scope="session")
+
+@pytest.fixture
 def app(request):
-    fixture = Application()
-    fixture.session.login(username="admin", password="admin")
+    global fixture
+    if fixture is None:
 
+        fixture = Application()
+        fixture.session.login(username="admin", password="admin")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def remove_session():
         fixture.session.logout()
         fixture.destroy()
