795227c02dbe41e689da2eb0ac7de78b36231928
avsnisar <avsnisar@ukr.net>
2019-07-12 03:34:14 +0300

    removed old unused functions

--- test_login.py
+++ test_login.py
@@ -1,6 +1,5 @@
 import unittest
 
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 from Application import Application
@@ -18,24 +17,6 @@ class Untitled(unittest.TestCase):
     def test_untitled(self):
         self.app.open_home_page()
         self.app.login("admin", "admin")
-
-    def is_element_present(self, how, what):
-        try:
-            self.app.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.app.wd.switch_to.alert
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert.text
-        finally:
-            self.accept_next_alert = True
 
     def tearDown(self):
         self.app.destroy()
